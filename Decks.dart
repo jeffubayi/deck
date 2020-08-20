7932eac452b26e31eb9844add72f69db5346d799
jeffubayi <jeffubayi@gmail.com>
2020-08-20 08:22:12 +0300

    Cards - create a list of cards and shuffle the deck

--- Decks.dart
+++ Decks.dart
@@ -1,21 +1,60 @@
-//create a deck of cards
-main(){
+//execute the app and display a deck of cards
+void main() {
   var deck = new Deck();
-  print(deck);
+  deck.shuffle();
+  print(deck.cardsWithSuit('Clubs'));
 }
 
+//create a list/deck of cards
 class Deck {
-  List cards = [];
+  List <Card> cards = [];
   
   Deck(){
-    var suits = ['Ace','Two','Three','Four','Five'];
-    var ranks = ['Diamonds','Hearts','Clubs','Spades'];
-    
+    var ranks = ['Ace','Two','Three','Four','Five'];
+    var suits = ['Diamonds','Hearts','Clubs','Spades'];
+//loop through the cards with for in    
     for(var rank in ranks){
       for(var suit in suits){
         var card = new Card(rank,suit);
         cards.add(card);
       }
     }
+  }
+//convert the deck of cards to string
+  toString(){
+    return cards.toString();
+  }
+
+//shuffle the cards in the deck
+  shuffle(){
+    cards.shuffle();
+  }
+
+  cardsWithSuit(String suit){
+    return cards.where((card) =>card.suit == suit);
+  }
+
+//Returns a new list containing the objects from start inclusive to end exclusive.
+  deal(int handSize){
+    var hand = cards.sublist(0,handSize);
+    cards = cards.sublist(handSize);
+    return hand;
+  }
+
+//removes both cards from a deck
+  removeCard(String suit, String rank){
+    cards.removeWhere((card)=>(card.suit == suit) && (card.rank == rank));
+    }
+  }
+
+//replace both cards as strings
+class Card {
+  String suit;
+  String rank;
+
+  Card(this.rank, this.suit);
+
+  toString(){
+    return '$rank of $suit';
   }
 }
